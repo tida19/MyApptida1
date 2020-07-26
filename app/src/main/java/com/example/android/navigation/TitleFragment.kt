b01dcccde2fb65e647c36a9aa53e5fd5eba67edd
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2020-07-27 00:19:31 +0700

    3.1.4-Step 1: Add a fragment class

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -31,8 +31,9 @@ class TitleFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false)
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
+                R.layout.fragment_title,container,false)
+        return binding.root
     }
 
     companion object {
