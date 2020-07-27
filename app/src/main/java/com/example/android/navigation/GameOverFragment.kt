e28971c3711fc860a8a10f935aab799ccfd955ce
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2020-07-27 14:20:55 +0700

    3.3.5-Step 2: Build and call an implicit intent

--- app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -33,7 +33,7 @@ class GameOverFragment : Fragment() {
                 inflater, R.layout.fragment_game_over, container, false)
         binding.tryAgainButton.setOnClickListener{view: View->
             view.findNavController()
-                    .navigate(GameOverFragmentDirections.actionGameOverFragmentToGameFragment())}
+                    .navigate(GameOverFragmentDirections.actionGameOverFragmentToTitleFragment())}
         return binding.root
     }
 }
